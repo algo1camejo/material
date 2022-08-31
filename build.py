67a4cd88b1e190a511d8aea0fd7df6b9a3cb0fcc
Manuel Bilbao <bilbaomanuel98@gmail.com>
2022-08-31 20:26:55 -0300

    Separadas teoricas y practicas

--- build.py
+++ build.py
@@ -5,13 +5,19 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('index.html')
 
-archivos = []
-for archivo in os.listdir("material"):
-    archivos += [archivo]
 
-archivos.sort()
+practicas = []
+for archivo in os.listdir("material/practicas"):
+    practicas += [archivo]
 
-output = template.render({"archivos": archivos})
+teoricas = []
+for archivo in os.listdir("material/teoricas"):
+    teoricas += [archivo]
+
+teoricas.sort()
+practicas.sort()
+
+output = template.render({"teoricas": teoricas, "practicas": practicas})
 
 os.system("mkdir -p _build")
 with open("_build/index.html", "w") as f:
